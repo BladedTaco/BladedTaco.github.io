7229cfa002f22ed1603a160fa7019dd7fd64b570
Jesse Linossier <25556311+BladedTaco@users.noreply.github.com>
2023-11-20 01:17:36 +1100

    Added more Banners

--- index.js
+++ index.js
@@ -10,16 +10,17 @@ embeds.forEach(embed => {
 
         // Close all embeds
         embeds.forEach(el => {
-            el.querySelector('iframe').src = "about:blank";
+            const iframe = el.querySelector('iframe');
+            if (iframe) iframe.src = "about:blank";
             el.classList.remove("shown")
         });
-        
+
         // Enable this Project Embed
         const iframe = embed.querySelector('iframe');
-        iframe.src = iframe.dataset.src
+        if (iframe) iframe.src = iframe.dataset.src;
         embed.classList.add("shown")
         
         // Smooth scroll into view once expanded
-        setTimeout(() => iframe.scrollIntoView({behavior: "smooth"}), 500);
+        setTimeout(() => embed.scrollIntoView({behavior: "smooth"}), 500);
     })
 })
